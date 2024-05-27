de1f4dcd911f2a9d90fa15da175a35f81bb7a4d7
sunzhqr <sanzhar_myrzash@mail.ru>
2024-05-28 01:04:15 +0500

    Variables and constans first try

--- 1/main.go
+++ 1/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() { //Entry point in application
-	fmt.Println("Hello, GO!")
+	//Initialization and assignment value of variable
+	message1 := "Hello, GO!" //variable := "something"
+
+	message2 := ""
+	message2 = "Hello, Golang!"
+
+	var message3 string
+	message3 = "Hello, Golang!"
+
+	//Constant variable
+	const Pi float64 = 3.1415926
+	const name = "Sanzhar"
+	fmt.Println(Pi)
+	fmt.Println(message1, message2, message3)
 }
