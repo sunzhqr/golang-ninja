8f8c29083e535892b0abefe07e581b8660458e01
sunzhqr <sanzhar_myrzash@mail.ru>
2024-05-28 23:13:09 +0500

    Type function

--- 1/main.go
+++ 1/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() { //Entry point in application
 	//Initialization and assignment value of variable
 	message1 := "Hello, GO!" //variable := "something"
 
 	message2 := ""
+	// Assign value
 	message2 = "Hello, Golang!"
 
 	var message3 string
 	message3 = "Hello, Golang!"
 
+	// Initialization
+	var num int
+	fmt.Println(reflect.TypeOf(num))
 	//Constant variable
 	const Pi float64 = 3.1415926
 	const name = "Sanzhar"
