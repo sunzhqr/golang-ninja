5d6cac0b86caaa8110e03e6e1fd01cdfde86791c
sunzhqr <sanzhar_myrzash@mail.ru>
2024-06-01 00:10:18 +0500

    If-else if-else

--- 1/main.go
+++ 1/main.go
@@ -3,16 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	message, _ := enterTheClub(17)
+	message, _ := enterTheClub(7)
 	fmt.Println(message)
 	//fmt.Println(entered)
 	//fmt.Println(enterTheClub(17))
 }
 
 func enterTheClub(age int) (string, bool) {
-	if age >= 18 {
+	if age >= 18 && age < 45 {
 		return "You accepted a club!", true
-	} else {
-		return "You cannot enter to the Club(", false
+	} else if age >= 45 && age <= 65 {
+		return "Are you sure?", true
+	} else if age >= 65 {
+		return "Not for you!", false
 	}
+	return "You cannot enter to the Club(", false
 }
