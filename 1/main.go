f4e9d83636b2a420ecab2d523b247aebc7f7773e
sunzhqr <sanzhar_myrzash@mail.ru>
2024-06-01 00:21:03 +0500

    Error handling, log.Fatal()

--- 1/main.go
+++ 1/main.go
@@ -1,21 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func main() {
-	message, _ := enterTheClub(7)
+	message, err := enterTheClub(27)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
-	//fmt.Println(entered)
-	//fmt.Println(enterTheClub(17))
 }
 
-func enterTheClub(age int) (string, bool) {
+func enterTheClub(age int) (string, error) {
 	if age >= 18 && age < 45 {
-		return "You accepted a club!", true
+		return "You accepted a club!", nil
 	} else if age >= 45 && age <= 65 {
-		return "Are you sure?", true
+		return "Are you sure?", nil
 	} else if age >= 65 {
-		return "Not for you!", false
+		return "Not for you!", errors.New("you are too old!")
 	}
-	return "You cannot enter to the Club(", false
+	return "You cannot enter to the Club(", errors.New("you are too young!")
 }
