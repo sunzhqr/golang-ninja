aba6e1a408485f40b467d01674b33c6d8c2f7093
sunzhqr <sanzhar_myrzash@mail.ru>
2024-06-04 00:25:40 +0500

    findMean fuction that receives unlimited count of numbers and return the min number

--- 1/main.go
+++ 1/main.go
@@ -1,47 +1,22 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	response, err := predictionForAbylay("Saturday")
-	if err != nil {
-		fmt.Println("Error", "WTF, you can't enter the day of Week?")
-	} else {
-		fmt.Println(response)
-	}
+	fmt.Println(findMin(1, 2, 3, 1, -65, 4, 64, 5, 453, 1, 351, 1, 531, 5))
 }
 
-func enterTheClub(age int) (string, error) {
-	if age >= 18 && age < 45 {
-		return "You accepted a club!", nil
-	} else if age >= 45 && age <= 65 {
-		return "Are you sure?", nil
-	} else if age >= 65 {
-		return "Not for you!", errors.New("you are too old!")
+func findMin(numbers ...int) int {
+	if len(numbers) == 0 {
+		return 0
 	}
-	return "You cannot enter to the Club(", errors.New("you are too young!")
-}
 
-func predictionForAbylay(dayOfWeek string) (string, error) {
-	switch dayOfWeek {
-	case "Monday":
-		return "Statistics fx", nil
-	case "Tuesday":
-		return "", nil
-	case "Wednesday":
-		return "Academic fx", nil
-	case "Thursday":
-		return "AP fx", nil
-	case "Friday":
-		return "Alem exam lose", nil
-	case "Saturday":
-		return "Military double dive into Abylay", nil
-	case "Sunday":
-		return "Othodnyak", nil
-	default:
-		return "Abylay wins", errors.New("Err")
+	min := numbers[0]
+
+	for _, i := range numbers {
+		if i < min {
+			min = i
+		}
 	}
+	return min
 }
