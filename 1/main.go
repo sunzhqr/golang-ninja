24fa42890059fc316961555615c564f2afda01bd
smyrzash <sanzhar_myrzash@mail.ru>
2024-06-23 20:48:17 +0500

    recover()

--- 1/main.go
+++ 1/main.go
@@ -3,24 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	defer printMeassage() //3
-	defer greeting()      // 2
-	fmt.Println("main()") // 1
-	// friends := []string{
-	// 	"Abylay",
-	// 	"Aigali",
-	// 	"Nur-Assyl",
-	// }
-	// friends[3] = "Tamerlan"
+	defer handlePanic()
 
-	// fmt.Println(friends)
-	panic("i get a fuck") // last or 4
+	friends := []string{
+		"Abylay",
+		"Aigali",
+		"Nur-Assyl",
+	}
+	fmt.Println("before panic")
+	fmt.Println(friends)
+	friends[3] = "Tamerlan"
+	fmt.Println(friends)       // not showed
+	fmt.Println("after panic") //not showed
+
 }
 
-func printMeassage() {
-	fmt.Println("printMessage()")
-}
-
-func greeting() {
-	fmt.Println("Hello from greeting()")
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+	}
+	fmt.Println("Panic handled succesfully!")
 }
