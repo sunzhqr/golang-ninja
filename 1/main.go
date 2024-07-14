bbeb81c7dde092d23398b579a627927cce1f7a40
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-14 14:53:21 +0500

    struct

--- 1/main.go
+++ 1/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+type User struct {
+	name   string
+	age    int
+	gender string
+}
+
 func main() {
-	func() {
-		fmt.Println("Anonymous function")
-	}()
+	user1 := User{"Sanzhar", 19, "Male"}
+	user2 := User{"Aigali", 19, "Male"}
+	fmt.Printf("%+v\n", user1)
+	fmt.Printf("%+v\n", user2)
 }
