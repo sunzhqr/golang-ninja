8098eb3b94fee23c797de91d9c6ab248b4616a0c
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-14 15:28:52 +0500

    constructor

--- 1/main.go
+++ 1/main.go
@@ -8,9 +8,17 @@ type User struct {
 	gender string
 }
 
+func NewUser(name, gender string, age int) User {
+	return User{
+		name:   name,
+		age:    age,
+		gender: gender,
+	}
+}
+
 func main() {
-	user1 := User{"Sanzhar", 19, "Male"}
+	user1 := NewUser("Sanzhar", "Male", 19)
 	user2 := User{"Aigali", 19, "Male"}
-	fmt.Printf("%+v\n", user1)
-	fmt.Printf("%+v\n", user2)
+	fmt.Printf("%+v\n", user1.name)
+	fmt.Printf("%+v\n", user2.age)
 }
