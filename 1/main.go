f74250a6a8c3574ac5814f0d49283b977b8c7766
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-15 22:31:05 +0500

    type switch

--- 1/main.go
+++ 1/main.go
@@ -26,12 +26,32 @@ func (c Circle) Area() float32 {
 }
 
 func main() {
-	square := Square{5}
+	//square := Square{5}
 	circle := Circle{5}
-	printArea(square)
-	printArea(circle)
+	//printArea(square)
+	//printArea(circle)
+	printInterface(Circle{5}) //unknown
+	var s struct{}
+	printInterface(s) //struct
+	printInterface("Sanzhar")
+	printInterface(circle)
 }
 
 func printArea(shape Shape) {
 	fmt.Println(shape.Area())
+}
+
+func printInterface(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("int")
+	case struct{}:
+		fmt.Println("struct")
+	case string:
+		fmt.Println("string")
+	case float32:
+		fmt.Println("float")
+	default:
+		fmt.Println("unknown")
+	}
 }
